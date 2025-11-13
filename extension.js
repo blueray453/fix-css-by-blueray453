e027a28535687af59fd23f4d75155c914d827630
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-14 00:52:16 +0600

    Alhamdulillah, adding notification colors

--- extension.js
+++ extension.js
@@ -1,9 +1,44 @@
-export default class FixCSS {
+import Gio from 'gi://Gio';
+import Clutter from 'gi://Clutter';
+import St from 'gi://St';
+import * as MessageTray from 'resource:///org/gnome/shell/ui/messageTray.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+export default class NotificationThemeExtension extends Extension {
+  constructor(metadata) {
+    super(metadata);
+    this._themeSignalId = null;
+  }
+
   enable() {
+    // Main.notify('My Extension', 'This is a notification from my GNOME extension!');
+    // global.notify_error("msg", "details");
     // Nothing to do; stylesheet.css handles everything
+    const messageTrayContainer = Main.messageTray.get_first_child();
+
+    this._themeSignalId = messageTrayContainer?.connect("child-added", () => {
+      const notificationContainer = messageTrayContainer?.get_first_child();
+      const notification = notificationContainer?.get_first_child();
+
+      const header = notification?.get_child_at_index(0);
+      const headerContent = header?.get_child_at_index(1);
+      const headerContentTime = headerContent?.get_child_at_index(1);
+
+      if (headerContentTime) {
+        // Set time color to red (RGB: 255, 0, 0, alpha: 1.0)
+        headerContentTime.set_style('color: #3f3f3f;');
+      }
+    });
+
   }
 
   disable() {
-    // Nothing to clean up
+    if (this._themeSignalId) {
+      const messageTrayContainer = Main.messageTray.get_first_child();
+      messageTrayContainer?.disconnect(this._themeSignalId);
+      this._themeSignalId = null;
+    }
   }
 }
