26ce54f680fe54b1ea84b09f4f704e23504c9993
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-28 19:17:04 +0600

    Alhamdulillah, remove notification related code

--- extension.js
+++ extension.js
@@ -17,10 +17,6 @@ export default class NotificationThemeExtension extends Extension {
   }
 
   enable() {
-    // Main.notify('My Extension', 'This is a notification from my GNOME extension!');
-    // global.notify_error("msg", "details");
-    // Nothing to do; stylesheet.css handles everything
-
     setLogFn((msg, error = false) => {
       let level;
       if (error) {
@@ -226,34 +222,6 @@ export default class NotificationThemeExtension extends Extension {
     DateMenu._calendar._weekStart = 6; // Saturday
 
     DateMenu._calendar._onSettingsChange();
-
-    const messageTrayContainer = Main.messageTray.get_first_child();
-
-    this._themeSignalId = messageTrayContainer?.connect("child-added", () => {
-      const notificationContainer = messageTrayContainer?.get_first_child();
-      const notification = notificationContainer?.get_first_child();
-
-      const header = notification?.get_child_at_index(0);
-      const headerContent = header?.get_child_at_index(1);
-      const headerContentSource = headerContent?.get_child_at_index(0);
-      const headerContentTime = headerContent?.get_child_at_index(1);
-
-      const content = notification?.get_child_at_index(1);
-      const contentContent = content?.get_child_at_index(1);
-      const contentContentTitle = contentContent?.get_child_at_index(0);
-      const contentContentBody = contentContent?.get_child_at_index(1).get_first_child();
-
-      headerContentTime.destroy();
-
-      // const bgColor = notificationContainer.get_theme_node().get_background_color();
-      // const bgColorHex = this.coglColorToHex(bgColor);
-
-      // headerContentTime.set_style(`color: ${bgColorHex};`);
-      // // headerContentSource.set_style('color: #00ff00;');
-      // // contentContentTitle.set_style('color: #ffff00;');
-      // // contentContentBody.set_style('color: #0000ff;');
-      // // notificationContainer.set_style('background-color: #6a0dad; border-radius: 12px;');
-    });
   }
 
   // watchPanelBox(boxType, itemOrder, delay = 1000) {
@@ -346,12 +314,6 @@ export default class NotificationThemeExtension extends Extension {
     PanelSessionMode.center.push(...this.centerItems);
 
     Panel._updatePanel();
-
-    if (this._themeSignalId) {
-      const messageTrayContainer = Main.messageTray.get_first_child();
-      messageTrayContainer?.disconnect(this._themeSignalId);
-      this._themeSignalId = null;
-    }
 
     DateMenu._calendar._weekStart = Shell.util_get_week_start();
     DateMenu._calendar._onSettingsChange();
